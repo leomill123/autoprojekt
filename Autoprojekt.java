61000997683c65d5802b064094cc9fd854e18a49
AlexKannNix <157794026+AlexKannNix@users.noreply.github.com>
2024-01-26 10:50:27 +0100

    Update Autoprojekt.java

--- Autoprojekt.java
+++ Autoprojekt.java
@@ -1,37 +1,49 @@
-/** 1.) Informieren
- *  a) Was ist eine Klasse? Eine Vorlage zum erzeugen von Objekten
- *  b) Was ist ein Objekt? Sind Instanzen von Klassen
- *  c) Was ist ein UML-Klassendiagram? Ein Strukturdiagram, welches die Beziehungen von Klassen
- *  grafisch darstellt.
- *  d) Was ist ein UML-Objektdiagram? Stellt Programmablauf zu einem konkreten Zeitpunkt dar
- *  e) Unterschied zwischen den beiden? Das eine stellt den Aufbau der Objekte dar
- *  und das andere wie die Objekte miteinander interagieren.
- *  f) Was ist ein Attribut eines Objektes? Man benutzt Attribute um Klassen zu beschreiben.
- *  g) Was ist ein Datentyp? Zeigt die Art von Wert einer Variablen an.
- *  h) Welche kennen wir? Integer, Double, short, long, char, string, boolean, float
- *  i) Welche Sichtbarkeiten gibt es? Private, public, protected
- *  j) Wozu dienen Methoden in Objekten? Eine Funktion, die etwas an Objekten macht.
- *  k) Was ist ein Parameter? Manche Methoden brauchen ein Input um die Funktion durchzuführen
- *  und diese werden als Parameter bezeichnet.
- *  l) Was ist ein Rückgabewert? Manche Methoden geben einen Wert zurück und dieser wird als
- *  Rückgabewert bezeichnet.
- *  m) Was versteht man unter Datenkapslung? Bewusste Verbergung von Daten von dem Zugriff von Außen.
- *  n) Wie wird der Zugriff auf private Attribute geregelt? Indem man eine Methode erstellt, die
- *  die Variable auslest und zurückgibt.
- *  o) Was ist ein Konstruktor? Wird verwendet um neues Objekt in der Klasse zu erstellen.
- *  p) Welche Attribute (inkl. Datentypen) benötiget man für ein Fahrzeug? String Hersteller,
- *  double Kilometerstand, double Verbrauch, double Tankinhalt, double Tankhöhe, double BasisMietpreis,
- *  double MietpreisProTag, Boolean leihStatus, int Leihdauer, String FahrzeugKlasse
- *  q) leihStatus braucht boolean (ja/nein) als Parameter; Tankhöhe braucht Rückgabewert; um die Tankhöhe
- *  zurückzugeben; (mehr kommt in Code, vertu)
- *  r) Um ein Objekt zu erstellen kann man in der main-method ein Objekt erstellen Beispiel:
- *  private Auto audi1 = new Auto();
- *  s) Man kann dies durch die Punktnotation machen, Beispiel:
- *  audi1.leihStatus();
- *  t) Worin unterscheidet sich die objektorientierte von der reinen 
-    strukturierten Programmierung? Bei der objektorientierten Programmierung hat man mehrere
-    Klasse auf die andere Klasse zugreifen können. Bei strukturierten Programmieren, wie z.B
-    in C, benutzt mans stattdessen oft Konstrukturen.
+1.) Informieren
+a) Was ist eine Klasse? 
+Eine Vorlage zum erzeugen von Objekten
+b) Was ist ein Objekt? 
+Sind Instanzen von Klassen
+c) Was ist ein UML-Klassendiagram? 
+Ein Strukturdiagram, welches die Beziehungen von Klassengrafisch darstellt.
+d) Was ist ein UML-Objektdiagram? 
+Stellt Programmablauf zu einem konkreten Zeitpunkt dar
+e) Unterschied zwischen den beiden? 
+Das eine stellt den Aufbau der Objekte dar und das andere wie die Objekte miteinander interagieren.
+f) Was ist ein Attribut eines Objektes? 
+Man benutzt Attribute um Klassen zu beschreiben.
+g) Was ist ein Datentyp? 
+Zeigt die Art von Wert einer Variablen an.
+h) Welche kennen wir? 
+Integer, Double, short, long, char, string, boolean, float
+i) Welche Sichtbarkeiten gibt es? 
+Private, public, protected
+j) Wozu dienen Methoden in Objekten?
+Eine Funktion, die etwas an Objekten macht.
+k) Was ist ein Parameter? 
+Manche Methoden brauchen ein Input um die Funktion durchzuführen und diese werden als Parameter bezeichnet.
+l) Was ist ein Rückgabewert? 
+Manche Methoden geben einen Wert zurück und dieser wird alsRückgabewert bezeichnet.
+m) Was versteht man unter Datenkapslung? 
+Bewusste Verbergung von Daten von dem Zugriff von Außen.
+n) Wie wird der Zugriff auf private Attribute geregelt? 
+Indem man eine Methode erstellt, die
+ie Variable auslest und zurückgibt.
+o) Was ist ein Konstruktor? 
+Wird verwendet um neues Objekt in der Klasse zu erstellen.
+p) Welche Attribute (inkl. Datentypen) benötiget man für ein Fahrzeug? String Hersteller,
+double Kilometerstand, double Verbrauch, double Tankinhalt, double Tankhöhe, double BasisMietpreis,
+double MietpreisProTag, Boolean leihStatus, int Leihdauer, String FahrzeugKlasse
+q) Welche "Fähigkeiten" (Methoden) benötigen Parameter und / oder Rückgabewerte? Wozu werden diese benötigt?
+leihStatus braucht boolean (ja/nein) als Parameter; Tankhöhe braucht Rückgabewert; um die Tankhöhe
+zurückzugeben; (mehr kommt in Code, vertu)
+r) Wie erzeugt man in JAVA aus einer Klasse ein Objekt (auch Beispiel angeben)?
+Um ein Objekt zu erstellen kann man in der main-method ein Objekt erstellen Beispiel:
+private Auto audi1 = new Auto();
+s) Wie greift man auf Methoden eines Objekts zu (auch Beispiel angeben)?
+Man kann dies durch die Punktnotation machen, Beispiel:
+audi1.leihStatus();
+t) Worin unterscheidet sich die objektorientierte von der reinen strukturierten Programmierung? 
+Bei der objektorientierten Programmierung hat man mehrere Klasse auf die andere Klasse zugreifen können. Bei strukturierten Programmieren, wie z.B in C, benutzt mans stattdessen oft Konstrukturen.
 
  
  * 
